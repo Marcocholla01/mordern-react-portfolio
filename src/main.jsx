6914da070264088b75fce2fd455a09ebfbbca613
Marcocholla01 <marcochollapaul01@gmail.com>
2024-05-20 01:03:56 +0300

    "Added Resume page and route to router"

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import About from "./pages/About.jsx";
+import Resume from "./pages/Resume.jsx";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <About />,
+      },
+      {
+        path: "/resume",
+        element: <Resume />,
       },
     ],
   },
