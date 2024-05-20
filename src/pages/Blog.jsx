606b1f326c49e79b97e66ce0c681e6501646cfa5
Marcocholla01 <marcochollapaul01@gmail.com>
2024-05-20 03:38:25 +0300

    "Added state and effect to fetch blog data, updated Blog component to render blog posts"

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FaRegEye } from "react-icons/fa";
+import BlogPost from "../components/BlogPost";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch(`blogs.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setBlogs(data);
+      });
+  }, []);
+
+  return (
+    <section className="blog">
+      <header>
+        <h2 className="h2 article-title">Blog</h2>
+      </header>
+      <div className="blog-posts">
+        <ul className="blog-posts-list">
+          {blogs.map((post, index) => (
+            <BlogPost key={index} post={post} />
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Blog;
